20c30d26fdfe4aa68d2d00c8992c792e0e60bff7
profinch <profinch@gmail.com>
2023-06-14 00:56:44 +0300

    Added Wordpress useful features

---
+++ wordpress/index.php
@@ -0,0 +1,96 @@
+
+# Display current song (update in 1 minute)
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<span style="font-weight:bold">Current song:</span>
+<span style="color:#ff7831; white-space: nowrap;">
+  <script type="text/javascript">
+    $(document).ready(function() {
+      function refreshDiv() {
+        $('#refresh').fadeOut(500, function() {
+          $('#refresh');
+        }).fadeIn(500);
+      }
+      setInterval(refreshDiv, 60000);
+    });
+  </script>
+  <div id="refresh"><?php get_template_part('radio', 'function') ?></div>
+</span>
+
+
+# Display current amount of listeners (update in 5 seconds)
+<span style="font-weight:bold">Listeners:</span>
+<span style="color:#ff7831;">
+<span id="song-listeners"></span>
+<script>
+  var timeout = 5;
+  function getStats(){
+    $.ajax({
+      url: "https://onair.example.com/status-json.xsl?mount=/live",
+      success: function( response ) {
+        $('#song-listeners').text(response.icestats.source.listeners)
+      }
+    });
+  }
+  getStats();
+  setInterval(getStats, timeout * 1000);
+</script>
+
+
+# Display maximum peak of listeners (update in 5 seconds)
+(<b>max:</b> <span id="song-listener_peak"></span>)
+<script>
+  var timeout = 5;
+  function getStats(){
+    $.ajax({
+      url: "https://onair.example.com/status-json.xsl?mount=/live",
+      success: function( response ) {
+        $('#song-listener_peak').text(response.icestats.source.listener_peak)
+      }
+    });
+  }
+  getStats();
+  setInterval(getStats, timeout * 1000);
+</script>
+
+
+# Copy the text by click
+<style>
+.copy-address {
+  cursor: pointer;
+  margin: 0 6px 0 0;
+  float: right;
+}
+
+.copy-address-tip {
+  position: relative;
+  margin: -18px 380px 0 0;
+}
+
+.copy-address-tip .copy-address-tip-text {
+  width: 90px;
+  background-color: #adadad;
+  color: #666666;
+
+  text-align: center;
+  padding: 1px 0;
+  border-radius: 2px;
+
+  position: absolute;
+  z-index: 1;
+  /* opacity: 0.5; */
+}
+</style>
+
+<script>
+function setClipboard(value) {
+  var tempInput = document.createElement("input");
+  tempInput.value = value;
+  document.body.appendChild(tempInput);
+  tempInput.select();
+  document.execCommand("copy");
+  document.body.removeChild(tempInput);
+  $('<div class="copy-address-tip"><span class="copy-address-tip-text">copied</span></div>').insertAfter('.copy-address').delay(1000).fadeOut(1000, function(){$(this).remove();});
+}
+</script>
+
+<img src="<?php bloginfo('template_url'); ?>/images/copy.svg" class="copy-address" alt="Copy" title="Click to copy" width="20" onclick="setClipboard('text-to-copy')">
---
+++ wordpress/radio-function.php
@@ -0,0 +1,41 @@
+<?php
+  set_error_handler(function ($level, $message, $file = '', $line = 0) {
+    throw new ErrorException($message, 0, $level, $file, $line);
+  });
+
+  function getMp3StreamTitle($streamingUrl, $interval, $offset = 0, $headers = true) {
+    $required = 'StreamTitle=';
+    $user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36';
+    $options = [
+      'http' => [
+        'method' => 'GET',
+        'header' => 'Icy-MetaData: 1',
+        'user_agent' => $user_agent
+      ]
+    ];
+    if (($headers = get_headers($streamingUrl)))
+      foreach ($headers as $h)
+        if (strpos(strtolower($h), 'icy-metaint') !== false && ($interval = explode(':', $h)[1]))
+          break;
+        $context = stream_context_create($options);
+        if ($stream = fopen($streamingUrl, 'r', false, $context)) {
+          while($buffer = stream_get_contents($stream, $interval, $offset)) {
+            if (strpos($buffer, $required) !== false) {
+              fclose($stream);
+            $title = explode($required, $buffer)[1];
+            return substr($title, 1, strpos($title, ';') - 2);
+            }
+            $offset += $interval;
+          }
+        }
+  }
+
+  try {
+    print(getMp3StreamTitle('https://onair.example.com/live', 443));
+  }
+
+  catch (Throwable $e) {
+    echo '<span style="color:#ff7831;">radio stream unavailable</span>';
+  }
+
+?>
